e162415b0c32f6f3a2bcb4c20a10e288f6533bad
Vlad Kampov <vladkampov@Kampov-MacBook-Pro-13.local>
2016-12-12 22:58:13 +0200

    Initial commit

---
+++ webpack.config.jsx
@@ -0,0 +1,61 @@
+webpack = require('webpack');
+path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+webpackConfig = {
+    context: __dirname,
+    entry: {
+        bundle: './src/jsx/app.jsx',
+        styles: './src/scss/main.scss'
+    },
+    output: {
+        filename: '[name].js',
+        path: './build',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: [/node_modules/],
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'stage-0', 'stage-1']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
+            }
+        ],
+        noParse: [
+          /aws\-sdk/,
+        ]
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['./'] }
+        }),
+        new ExtractTextPlugin('styles.css', {
+            allChunks: true
+        })
+    ],
+    devServer: {
+        historyApiFallback: true
+    }
+};
+
+module.exports = webpackConfig;
